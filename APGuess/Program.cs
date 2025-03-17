c8a9f4b0029f9309fafef2db0c7847758af526c1
danilo <danilocencigg@gmail.com>
2025-03-17 16:43:29 -0300

    refatorada em funções

--- APGuess/Program.cs
+++ APGuess/Program.cs
@@ -1,75 +1,110 @@
-﻿namespace APGuess
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace APGuess
 {
     internal class Program
     {
+        
+        static void MainProgram()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Jogo de Advinhação");
+            Console.WriteLine("--------------------\n");
+
+            int totalTries;
+            Console.WriteLine("Escolha o Nível de Dificuldade:\n\n1.Fácil (5 Tentativas)\n2.Médio (4 Tentativas)\n3.Difícil (3 Tentativas)\n---------------\n4.Sair\n");
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                Game(5);
+            }
+            else if (userInput == "2")
+            {
+                Game(4);
+            }
+            else if (userInput == "3")
+            {
+                Game(3);
+            }
+            else if (userInput == "4")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Opção Inváliada, Pressione Enter para retornar");
+                Console.ReadLine();
+                MainProgram();
+            }
+        }
+
+        
+        static int RandGenerator()
+        {
+            Random randGen = new Random();
+            int randNumber = randGen.Next(1, 30);
+
+            return randNumber;
+        }
+        
+        
+        
+        
+        static void Game(int maxTries)
+        {
+            Console.Clear();
+            int totalTries = maxTries;
+            int randomNumber = RandGenerator();
+            int tries;
+            
+            for (tries = 1; tries <= totalTries; tries++)
+            {
+                Console.WriteLine($"Tentativa {tries} de {totalTries}");
+                Console.WriteLine("Digite um Número entre 1 e 30:\n");
+                int playerNumber = int.Parse(Console.ReadLine());
+
+                if (playerNumber == randomNumber)
+                {
+                    Console.WriteLine("Você acertou! Deseja jogar novamente? (y/n)");
+                    string playerAnswer = Console.ReadLine();
+                    if (playerAnswer == "y")
+                    {
+                        Game(maxTries);
+                    } else if (playerAnswer == "n")
+                    {
+                        MainProgram();
+                    } else
+                    {
+                        Console.WriteLine("Opção Inválida");
+                        playerAnswer = Console.ReadLine();
+                    }
+                    
+                }
+                else if (playerNumber < randomNumber)
+                {
+                    Console.WriteLine($"O Número Sorteado é Maior que {playerNumber}");
+                }
+                else if (playerNumber > randomNumber)
+                {
+                    Console.WriteLine($"O Número Sorteado é Menor que {playerNumber}");
+                }
+
+                if (tries == totalTries)
+                {
+                    Console.WriteLine($"Tentativas Esgotadas, O Número Sorteado era {randomNumber}");
+                }
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine("--------------------");
-                Console.WriteLine("Advinhe o Número");
-                Console.WriteLine("--------------------\n");
-
-                int totalTries;
-                Console.WriteLine("Escolha o Nível de Dificuldade:\n1.Fácil (5 Tentativas)\n2.Médio (4 Tentativas)\n3.Difícil (3 Tentativas)\n");
-                string userInput = Console.ReadLine();
-                if (userInput == "1")
-                {
-                    totalTries = 5;
-                } else if (userInput == "2")
-                {
-                    totalTries = 4;
-                } else if (userInput == "3")
-                {
-                    totalTries = 3;
-                } else
-                {
-                    Console.WriteLine("Opção Inváliada");
-                    break;
-                }
+            MainProgram();
 
 
-                    Random randGen = new Random();
-                int randNumber = randGen.Next(1, 30);
 
-                int tries;
-                
-                for (tries = 1; tries <= totalTries; tries++)
-                {
-                    Console.WriteLine($"Tentativa {tries} de {totalTries}");
-                    Console.WriteLine("Digite um Número entre 1 e 30:\n");
-                    int playerNumber = int.Parse(Console.ReadLine());
 
-                    if (playerNumber == randNumber)
-                    {
-                        Console.WriteLine("Você acertou!");
-                        break;
-                    }
-                    else if (playerNumber < randNumber)
-                    {
-                        Console.WriteLine($"O Número Sorteado é Maior que {playerNumber}");
-                    }
-                    else if (playerNumber > randNumber)
-                    {
-                        Console.WriteLine($"O Número Sorteado é Menor que {playerNumber}");
-                    }
-
-                    if (tries == totalTries)
-                    {
-                        Console.WriteLine($"Tentativas Esgotadas, O Número Sorteado era {randNumber}");
-                    }
-                }
-                
-               
-                
-                Console.WriteLine("Pressione Enter para Sair");
-                string enq = Console.ReadLine().ToUpper();
-
-                if (enq != "S")
-                    break;
             }
-             
         }
     }
-}
